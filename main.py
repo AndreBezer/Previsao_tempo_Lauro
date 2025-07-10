662c9c73312f6f04f4fdb4e935387c8147f5b753
André Luis <andrelus475@gmail.com>
2025-07-09 21:31:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ import telebot
 import requests
 from time import sleep
 
-chave_bot = "7770862620:AAEKKvTTNkFHK4GG21ZxUK-3zYbpCrhQXZg"
-chave_API = "3854106ac8f16b66d9226ff557c5f947"
-dono = "6234485725"
+chave_bot = "chave_do_bot"
+chave_API = "chave_da_api"
+grupo = "codigo_do_grupo"
 
 bot = telebot.TeleBot(chave_bot)
 
-def mensagem():
-    sleep(10800)
-
+def responder_tempo():
     cidade = "Lauro de freitas"                                 
     link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={chave_API}&lang=pt_br"
 
@@ -25,7 +23,8 @@ def mensagem():
     else:
         tempo = f"Tempo: ☁️ {descricao}. ☁️\nTemperatura ☀️: {temperatura:.2f}°C ☀️"
 
-    bot.send_message(dono, tempo)
+    bot.send_message(grupo, tempo)
 
 while True:
-    mensagem()
+    responder_tempo()
+    sleep(10800)
